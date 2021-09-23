c875b7c201ae60983f879b7f5a6978bb809ddd7e
cjsstart <13887096914@163.com>
2021-09-24 02:29:58 +0800

    Swagger2测试通过

---
+++ mall-order/src/main/java/com/xyz/mall/order/config/SwaggerConfig.java
@@ -0,0 +1,52 @@
+package com.xyz.mall.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 陈进松
+ * @Date: 2021/09/24/1:51
+ * @Description:
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket docket(Environment environment){
+        Profiles dev = Profiles.of("dev");//开发环境开启
+        boolean isOpen = environment.acceptsProfiles(dev);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .groupName("cjsstart")
+                .enable(isOpen)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.xyz.mall.order.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo(){
+        Contact contact = new Contact("cjsstart","http://www.xyz.com","xxx@xyz.com");
+        ApiInfo apiInfo = new ApiInfo("商城的在线API文档"
+        ,"商城基础项目"
+                ,"v1.0"
+                ,"http://www.xyz.com"
+                ,contact
+                ,"Apache License2.0"
+                ,"http://www.apache.org/licenses/LICENSE-2.0.html"
+                ,new ArrayList<>());
+        return apiInfo;
+    }
+}
